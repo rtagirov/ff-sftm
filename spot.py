bf9bd527a12417693269edd6940480f13c14c07a
rtagirov <tagirovrinat@gmail.com>
2018-10-02 17:41:58 +0200

    modified version works

--- spot.py
+++ spot.py
@@ -21,13 +21,15 @@ import auxfunc
 importlib.reload(auxfunc)
 
 #setting up the grid for the calculations
-factor = 10.
-xvalues = np.around(np.linspace(0,359,num=360*factor),decimals=1)
-yvalues = np.around(np.linspace(0,180,num=181*factor),decimals=1)
-x,y = np.meshgrid(xvalues,yvalues)
+factor = 10.0
+
+xvalues = np.around(np.linspace(0.0, 359.0, num = 360.0 * factor), decimals = 1)
+yvalues = np.around(np.linspace(0.0, 180.0, num = 181.0 * factor), decimals = 1)
+
+x, y = np.meshgrid(xvalues, yvalues)
 
 #for doing the sin/cos calculations
-conv = np.pi/180.
+conv = np.pi / 180.0
 
 #load in the new input file that also contains the positions on the days of decay
 data = np.loadtxt("spot_evolution.inp")
@@ -49,8 +51,8 @@ area = data[:, 5]
 grid_max = 359
 
 #def f(i):
-#for i in tqdm(range(0, len(data[:, 0])), ncols = auxfunc.term_width(), desc = 'Masking spots'):
-for i in range(0, len(data[:, 0]):
+for i in tqdm(range(0, len(data[:, 0])), ncols = auxfunc.term_width(), desc = 'Masking spots'):
+#for i in range(0, len(data[:, 0])):
 
     positivx= open("positivx{}.txt".format(i), "w")
     positivy= open("positivy{}.txt".format(i), "w")
@@ -63,19 +65,21 @@ for i in range(0, len(data[:, 0]):
     r = np.sqrt(area[i])
 
     #for taking into account that pixels become smaller towards the pole
-    phi_pos = 90.0 - data[:, 1]
-    phi_neg = 90.0 - data[:, 3]
+#    phi_pos = 90.0 - data[:, 1]
+#    phi_neg = 90.0 - data[:, 3]
+    phi_pos = 90.0 - lat_pos[i]
+    phi_neg = 90.0 - lat_neg[i]
 
-    x_min_pos = long_pos[i] - r / 2. * 1. / np.cos(phi_pos*conv)
-    x_max_pos = long_pos[i] + r / 2. * 1. / np.cos(phi_pos*conv)
-    y_min_pos = lat_pos[i] - r / 2.
-    y_max_pos = lat_pos[i] + r / 2.
+    x_min_pos = long_pos[i] - r / 2.0 * 1.0 / np.cos(phi_pos * conv)
+    x_max_pos = long_pos[i] + r / 2.0 * 1.0 / np.cos(phi_pos * conv)
+    y_min_pos = lat_pos[i] - r / 2.0
+    y_max_pos = lat_pos[i] + r / 2.0
 
     #define negative polarity patch
-    x_min_neg = long_neg[i]-r/2.*1./np.cos(phi_neg*conv)
-    x_max_neg = long_neg[i]+r/2.*1./np.cos(phi_neg*conv)
-    y_min_neg = lat_neg[i]-r/2.
-    y_max_neg = lat_neg[i]+r/2.
+    x_min_neg = long_neg[i] - r / 2.0 * 1.0 / np.cos(phi_neg * conv)
+    x_max_neg = long_neg[i] + r / 2.0 * 1.0 / np.cos(phi_neg * conv)
+    y_min_neg = lat_neg[i] - r / 2.0
+    y_max_neg = lat_neg[i] + r / 2.0
 
     #the if-statements are just for the cases if part of the spots would be masked
     #outside the grid
